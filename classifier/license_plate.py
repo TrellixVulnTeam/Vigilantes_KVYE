903f8cae619b8bb191814cb3463e6c69c78b8d7c
akorot <akorot@umich.edu>
2021-10-20 14:49:28 -0400

    Updates to license plate stuff

--- classifier/license_plate.py
+++ classifier/license_plate.py
@@ -1,20 +1,26 @@
 import cv2
 import pytesseract
 import numpy as np
+import os
+from PIL import Image
 
 pytesseract.pytesseract.tesseract_cmd = r'/mnt/c/Program Files/Tesseract-OCR/tesseract.exe'
 
 def main():
-    img = cv2.imread('test_license_plate.jpg')
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    gray, img_bin = cv2.threshold(gray,128,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    gray = cv2.bitwise_not(img_bin)
-    kernel = np.ones((2, 1), np.uint8)
-    img = cv2.erode(gray, kernel, iterations=1)
-    img = cv2.dilate(img, kernel, iterations=1)
+    # img = cv2.imread('test_license_plate.png')
+    # gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # gray, img_bin = cv2.threshold(gray,128,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    # gray = cv2.bitwise_not(img_bin)
+    # kernel = np.ones((2, 1), np.uint8)
+    # img = cv2.erode(gray, kernel, iterations=1)
+    # img = cv2.dilate(img, kernel, iterations=1)
     print("======== Image text =======")
-    print(pytesseract.image_to_string(img, config='-psm 6'))
+    config_str = "-l eng --oem 4 --psm 7"
+    #print() #, lang = 'eng', config ='--psm 0 txt'))
+    img = Image.open('test.png')
+    print(pytesseract.image_to_string(img, lang='eng', config=("txt "+config_str)))
     print("===========================")
+    
 
 if __name__ == '__main__':
     main()
