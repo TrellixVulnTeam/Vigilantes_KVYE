fafa42f782740fba067cc6bfa0976443066fba02
wassinkl <wassinkl@umich.edu>
2021-10-23 23:12:04 -0400

    App crashes on view switch

--- project_frontend/app/src/main/java/edu/umich/Vigilantes/MainActivity.kt
+++ project_frontend/app/src/main/java/edu/umich/Vigilantes/MainActivity.kt
@@ -4,10 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
 import android.Manifest
+import android.content.Intent
+import android.view.View
+import edu.umich.Vigilantes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var view: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        view = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
@@ -16,5 +22,10 @@ class MainActivity : AppCompatActivity() {
                 finish()
             }
         }.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        view.idgotoPDF.setOnClickListener {
+            startActivity(Intent(this, pdfActivity::class.java))
+        }
     }
+
+    fun genPDF(view: View?) = startActivity(Intent(this, pdfActivity::class.java))
 }
