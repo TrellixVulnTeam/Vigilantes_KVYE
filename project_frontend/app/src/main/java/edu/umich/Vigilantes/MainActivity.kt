b825f30658839ae7130c428b68b421c5c948e46d
George Chu <44956995+whybox@users.noreply.github.com>
2021-10-23 20:32:09 -0400

    Get date and time

--- project_frontend/app/src/main/java/edu/umich/Vigilantes/MainActivity.kt
+++ project_frontend/app/src/main/java/edu/umich/Vigilantes/MainActivity.kt
@@ -4,11 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
 import android.Manifest
+import android.icu.text.SimpleDateFormat
+import android.widget.TextView
+import java.util.Date;
+
+
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.date)
+        val sdf = SimpleDateFormat("yyyy.MM.dd G 'at' HH:mm:ss z")
+        val currentDateandTime: String = sdf.format(Date())
+        textView.text = currentDateandTime
 
         registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
             if (!granted) {
