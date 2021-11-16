28b96be0d5996b448d9bd2edd14b47c8a179fba4
wassinkl <wassinkl@umich.edu>
2021-11-16 00:00:48 -0500

    Classifier Integrated with Activity

--- project_frontend/app/src/main/java/edu/umich/Vigilantes/reportStore.kt
+++ project_frontend/app/src/main/java/edu/umich/Vigilantes/reportStore.kt
@@ -2,21 +2,20 @@ package edu.umich.Vigilantes
 
 import android.content.Context
 import android.net.Uri
-import android.telecom.Call
-import android.util.Log
-import androidx.core.net.toFile
-import com.android.volley.RequestQueue
-import com.android.volley.Response
-import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.Volley.newRequestQueue
+import android.widget.TextView
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaType
+import java.io.IOException
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley.newRequestQueue
+import kotlin.reflect.full.declaredMemberProperties
+import okhttp3.*
 import okhttp3.RequestBody.Companion.asRequestBody
 import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
-import java.io.IOException
 import kotlin.reflect.full.declaredMemberProperties
+import androidx.appcompat.app.AppCompatActivity
 
 
 object reportStore {
@@ -25,14 +24,16 @@ object reportStore {
 
     private val client = OkHttpClient()
 
+    val carLabels = arrayListOf<Int>()
+
     fun postImages(context: Context, imageUri: Uri?,
-                  completion: (String) -> Unit) {
+                  completion: (String) -> Unit?) {
         val mpFD = MultipartBody.Builder().setType(MultipartBody.FORM)
         imageUri?.run {
-            toFile()?.let {
+            toFile(context)?.let {
                 mpFD.addFormDataPart("image", "carImage",
                     it.asRequestBody("image/jpeg".toMediaType()))
-            }
+            } ?: context.toast("Unsupported image format")
         }
 
         val request = Request.Builder()
@@ -40,13 +41,31 @@ object reportStore {
             .post(mpFD.build())
             .build()
 
-        context.toast("Posting . . . wait for 'Chatt posted!'")
+        context.toast("Posting . . .")
 
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                completion(e.localizedMessage ?: "Posting failed")
+            }
 
-        client.newCall(request).execute().use { response ->
-            if (!response.isSuccessful) throw IOException("Unexpected code $response")
-            println(response.body!!.string())
-        }
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    completion("Chatt posted!")
+                    //println(response.body!!.string())
+                    carLabels.clear()
+                    val cars = try { JSONObject(response.body?.string() ?: "").getJSONArray("prediction") } catch (e: JSONException) { JSONArray() }
+                    for(i in 0..2){
+                        carLabels.add(cars[i] as Int)
+                    }
+                    completion("Added car labels")
+                }
+                else{
+                    completion("Failed to predict car type... Is certificate installed?")
+                }
+            }
+        })
+
     }
+
 
 }
