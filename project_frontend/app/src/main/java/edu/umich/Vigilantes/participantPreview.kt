7f3a37fef087a38446edaa7f0e026181b7d4ba46
CHBChan <81986429+CHBChan@users.noreply.github.com>
2021-11-16 14:39:43 -0500

    Added delete confirmation pop ups

--- project_frontend/app/src/main/java/edu/umich/Vigilantes/participantPreview.kt
+++ project_frontend/app/src/main/java/edu/umich/Vigilantes/participantPreview.kt
@@ -1,15 +1,23 @@
 package edu.umich.Vigilantes
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.transition.Slide
 import android.util.Log
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.PopupWindow
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class participantPreview : AppCompatActivity() {
+    private var popupExists = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,11 +62,8 @@ class participantPreview : AppCompatActivity() {
         }
 
         deleteButton.setOnClickListener {
-            val intent = Intent()
             if (retrParticipant != null) {
-                intent.putExtra("Delete index", retrParticipant.position)
-                setResult(66, intent)
-                finish()
+                createPopUp(retrParticipant)
             }
         }
     }
@@ -85,4 +90,49 @@ class participantPreview : AppCompatActivity() {
             }
             finish()
         }
+
+    private fun createPopUp(retrParticipant: ParticipantInfo) {
+        //Initialize popup window
+        Log.d("createPopUp", "Calling createPopUp")
+        val inflater: LayoutInflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = inflater.inflate(R.layout.delete_popup_box, null)
+        val popupWindow = PopupWindow(
+            view,
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT)
+        //Set popup window on top of parent view
+        popupWindow.elevation = 10.0F
+
+        val slideIn = Slide()
+        slideIn.slideEdge = Gravity.START
+        popupWindow.exitTransition = slideIn
+
+        val slideOut = Slide()
+        slideOut.slideEdge = Gravity.RIGHT
+        popupWindow.exitTransition = slideOut
+
+        val deleteOpt = view.findViewById<Button>(R.id.DeleteOption)
+        val cancelOpt = view.findViewById<Button>(R.id.CancelOption)
+        //Delete option chosen
+        deleteOpt.setOnClickListener{
+            popupWindow.dismiss()
+            popupExists = false
+
+            val intent = Intent()
+            if (retrParticipant != null) {
+                intent.putExtra("Delete index", retrParticipant.position)
+                setResult(66, intent)
+                finish()
+            }
+        }
+        //Cancel option chosen
+        cancelOpt.setOnClickListener{
+            popupWindow.dismiss()
+            popupExists = false
+        }
+
+        //Display popup window
+        var root_layout = findViewById<ConstraintLayout>(R.id.root_layout)
+        popupWindow.showAtLocation(root_layout, Gravity.CENTER, 0, 0)
+    }
 }
